31f18ab7b9a526fe47908c957b4455478f250980
Kotchakorn Surin <kotchakorn.s@siamkubota.co.th>
2022-06-27 15:53:29 +0700

    success mapping

--- myWeb/myWeb/views.py
+++ myWeb/myWeb/views.py
@@ -1,73 +1,82 @@
+from genericpath import exists
 from telnetlib import KERMIT
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from database.models import Employee, EmployeeForm, Line
 import requests
 
+
 def index(request):
     user_id = '1234'
 
-    # if (request.POST.get('employee_id') not in Employee.objects.get('employee_id')):
-    #     messages.info(request, 'Already mapping')
-    #     return redirect('index')
-    if (str(user_id) in Line.objects.all()):
-        a = Line.objects.get(user_id=user_id)
-        # return object that connected with line_id
-        employee = Employee.objects.get(line=a)
-        nameEN = employee.employee_id
-        data = {
-            'nameEN': nameEN,
-        }
+    if (Line.objects.filter(user_id=user_id).exists()):
+        l = Line.objects.get(user_id=user_id)
+        # check EMP ID with DB
+        # if Employee.objects.filter(line=l).exists()==False:
+            
+        if Employee.objects.filter(line=l).exists():
+            employee = Employee.objects.get(line=l)
+            eid = employee.employee_id
 
-        return render(request, 'greet.html', data)
-
-    # if (str(user_id) not in Line.objects.all()):
-    #     l = Line(user_id=user_id)
-    #     l.save()
-    #     try:
-    #         Employee.objects.get(employee_id=request.POST.get('employee_id'))
-    #     except Employee.DoesNotExist:
-    #         if request.method == "POST":
-    #             form = EmployeeForm(request.POST)
-    #             if form.is_valid:
-    #                 pc = Employee(
-    #                     line = l,
-    #                     employee_id = request.POST.get('employee_id'),
-    #                 )
-    #                 pc.save()
-    #         else:
-    #             form = EmployeeForm()
-
-    #         return render(request, 'index.html', {'form': form})
-
-    #     # if (Employee.objects.get(employee_id=request.POST.get('employee_id')).exist):
-    #     messages.info(request, 'Already')
-    #     return redirect('index')
+            body = {
+                "UserName": "admin_ss",
+                "Password": "ss123456*"
+            }
+            getTokenURL = 'https://p701apsi01-la02skc.azurewebsites.net/skcapi/token'
+            x = requests.post(getTokenURL, json=body).json()
+            myToken = x["accessToken"]
+            getEmpURL = f"https://p701apsi01-la01skc.azurewebsites.net/skcapi/empid/{eid}"
+            auth = {"Authorization": "Bearer %s" %myToken}
+            y = requests.get(getEmpURL, headers=auth).json()
+            nameEN = y[0]["nameEN"]
+            
+            return render(request, 'greet.html', {'nameEN': nameEN,})
+    else:
+        # save LINE ID in DB
+        l = Line(user_id=user_id)
         
-    # else:    
-    #     a = Line.objects.get(user_id=user_id)
-    #     # return object that connected with line_id
-    #     employee = Employee.objects.get(line=a)
-    #     nameEN = employee.employee_id
-    #     data = {
-    #         'nameEN': nameEN,
-    #     }
+        if request.method == "POST":
+            form = EmployeeForm(request.POST)
+            eid = request.POST.get('employee_id')
+            p_id = request.POST.get('personal_id')
+            if form.is_valid:
+                # check that user type EMP ID in DB
+                if Employee.objects.filter(employee_id=eid).exists():
+                    messages.info(request, 'This Employee Id Already Used!')
+                else:
+                    try:
+                        body = {
+                            "UserName": "admin_ss",
+                            "Password": "ss123456*"
+                        }
+                        getTokenURL = 'https://p701apsi01-la02skc.azurewebsites.net/skcapi/token'
+                        x = requests.post(getTokenURL, json=body).json()
+                        myToken = x["accessToken"]
+                        getEmpURL = f"https://p701apsi01-la01skc.azurewebsites.net/skcapi/empid/{eid}"
+                        auth = {"Authorization": "Bearer %s" %myToken}
+                        y = requests.get(getEmpURL, headers=auth).json()
+                        nameEN = y[0]["nameEN"]
+                        per_id = int(y[0]["personal_Id"])
 
-    #     return render(request, 'greet.html', data)
+                        if (int(per_id) == int(p_id)):
+                            l.save()
+                            pc = Employee(
+                                line = l,
+                                employee_id = eid,
+                            )
+                            pc.save()
+                            return render(request, 'greet.html', {'nameEN': nameEN,})
+                        else:
+                            messages.info(request, 'Your Personal ID miss match')
+                    except KeyError as e:
+                        messages.info(request, 'No Employee ID in database!')
+        else:
+            form = EmployeeForm()
+        return render(request, 'index.html')
 
-      # ... handle the case of that user not existing
-        # if (Employee.objects.get(employee_id=request.POST.get('employee_id')).exist):
-                
-        #     messages.alert(request, 'Already')
-        #     a = Line.objects.get(user_id=user_id)
-        #     # return object that connected with line_id
-        #     employee = Employee.objects.get(line=a)
-        #     nameEN = employee.employee_id
-        #     data = {
-        #         'nameEN': nameEN,
-        #     }
-
-        #     return render(request, 'greet.html', data)
+        # if (Employee.objects.filter(employee_id=request.POST.get('employee_id')).exists()):
+        #     messages.info(request, 'Already mapping')
+        #     return redirect('index')
         # else:
         #     l = Line(user_id=user_id)
         #     l.save()
@@ -79,57 +88,12 @@ def index(request):
         #                 employee_id = request.POST.get('employee_id'),
         #             )
         #             pc.save()
+
+        #             # return redirect('greet')
         #     else:
         #         form = EmployeeForm()
 
         #     return render(request, 'index.html', {'form': form})
-    return render(request, 'index.html')
-
-    # try:
-    #     a = Line.objects.get(user_id=user_id)
-    #     # return object that connected with line_id
-    #     employee = Employee.objects.get(line=a)
-    #     nameEN = employee.employee_id
-    #     data = {
-    #         'nameEN': nameEN,
-    #     }
-    #     return render(request, 'greet.html', data)
-    # except Line.DoesNotExist:
-    #     l = Line(user_id=user_id)
-    #     l.save()
-    #     if request.method == "POST":
-    #         form = EmployeeForm(request.POST)
-    #         if form.is_valid:
-    #             pc = Employee(
-    #                 line = l,
-    #                 employee_id = request.POST.get('employee_id'),
-    #             )
-    #             pc.save()
-    #     else:
-    #         form = EmployeeForm()
-    # return render(request, 'index.html')
-
-    # if (user_id not in Line.objects.get(user_id=user_id)):
-    #     l = Line(user_id=user_id)
-    #     l.save()
-    #     if request.method == "POST":
-    #         form = EmployeeForm(request.POST)
-    #         if form.is_valid:
-    #             pc = Employee(
-    #                 line = l,
-    #                 employee_id = request.POST.get('employee_id'),
-    #             )
-    #             pc.save()
-    #     else:
-    #         form = EmployeeForm()
-    # else:
-    #     # return object that connected with line_id
-    #     employee = Employee.objects.get(user_id=user_id)
-    #     nameEN = employee.employee_id
-    #     data = {
-    #         'nameEN': nameEN,
-    #     }
-    #     return render(request, 'greet.html', data)
 
     # return render(request, 'index.html')
 
@@ -156,7 +120,7 @@ def greet(request):
                 'nameEN': nameEN,
             }
             form = Employee.objects.all()
-            return render(request, 'greet.html', {'form': form,'nameEN': nameEN,})
+            return render(request, 'greet.html', {'form': form, 'nameEN': nameEN,})
         else:
             print(p_id,per_id)
             messages.info(request, 'Your Personal ID miss match')
